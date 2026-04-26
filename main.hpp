5b1f55e59a6801dd30d5c6ec60736260fb03a814
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-04-25 20:42:12 -0700

    Updated main.hpp

--- main.hpp
+++ main.hpp
@@ -8,14 +8,23 @@ void swapValues(int &, int &, int &, int &);
 void swapValues(int &n1, int &n2)
 {
     // TODO
+    int tmp = n1;
+    n1 = n2;
+    n2 = tmp;
 }
 
 void swapValues(int &n1, int &n2, int &n3)
 {
     // TODO
+    int tmp = n1;
+    n1 = n2;
+    n2 = n3;
+    n3 = tmp;
 }
 
 void swapValues(int &n1, int &n2, int &n3, int &n4)
 {
     // TODO
+    swap(n1,n4);
+    swap(n2,n3);
 }
